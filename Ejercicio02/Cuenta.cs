c8b1e0a5c9ca7035431860bea65edc7213dab6a6
GonzaloBernardini <gbernardini58@gmail.com>
2022-05-09 02:03:00 -0300

    Ejercicios01/02/03/08 Completados

---
+++ Ejercicio02/Cuenta.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Ejercicio02.Program;
+
+namespace Ejercicio02
+{
+    class Cuenta : Persona
+    {
+        private Persona Titular { get; set; }
+
+        public decimal Cantidad_;
+        private decimal? Cantidad { 
+            get 
+            {
+                return Cantidad_;
+            } set
+            {
+                if (value > 0)
+                {
+                    Cantidad_ = Convert.ToDecimal(value);
+                }
+                else
+                {
+                    Console.WriteLine("Primero debe ingresar dinero");
+                }
+
+            } }
+
+
+
+        public Cuenta()
+        {
+
+        }
+        public void MostrarDatosCuenta()
+        {
+            if (Cantidad_ < 100)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"El titular de la cuenta es:{Titular}\nEl dinero actual en su cuenta es de : {Cantidad_}");
+            }
+            Console.WriteLine($"El titular de la cuenta es:{Titular}\nEl dinero actual en su cuenta es de : {Cantidad_}");
+
+        }
+        public decimal IngresarDinero(decimal a)
+        {
+            if (a < 0)
+            {
+                Console.WriteLine("No puede ingresar cantidades negativas");
+            }
+              
+            Cantidad_ = a;
+            return Cantidad_;
+        }
+
+        public decimal RetirarDinero(decimal b)
+        {
+           
+            if (b < 0)
+            {
+                Console.WriteLine("El valor debe ser mayor a 0");
+            }
+            decimal dineroFinal = Cantidad_ - b;
+            return Cantidad_ - dineroFinal;
+        }
+    }
+}
--- Ejercicio02/Program.cs
+++ Ejercicio02/Program.cs
@@ -6,9 +6,11 @@ namespace Ejercicio02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese nombre, luego edad y por ultimo DNI");
+            Console.WriteLine("Ingrese nombre");
             string nombr = Console.ReadLine();
+            Console.WriteLine("Ingrese edad");
             int ed = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese su DNI sin puntos.");
             string dniPer = Console.ReadLine();
             Persona nuevaPer = new Persona();
             nuevaPer.nombre = nombr;
@@ -16,8 +18,33 @@ namespace Ejercicio02
             nuevaPer.DNI = dniPer;
             nuevaPer.getDatos();
             nuevaPer.esMayorEdad();
+            Console.WriteLine("Desea crearse una cuenta? Ingrese 1 para crear : Ingrese 2 para salir");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+            if (valor1 == 1)
+            {
+                Cuenta n = new Cuenta();
+                Console.WriteLine("Ha creado su cuenta\nIngrese 1 para depositar dinero o 2 para retirar dinero");
+                int valor2 = Convert.ToInt32(Console.ReadLine());
+                if (valor2 ==1)
+                {
+                    Console.WriteLine("Ingrese el valor a depositar");
+                    decimal valorIngresado = Convert.ToDecimal(Console.ReadLine());
+                    n.IngresarDinero(valorIngresado);
+                    n.MostrarDatosCuenta();
+                }
+                else if(valor2 == 2)
+                {
+                    decimal valorRetiro = Convert.ToDecimal(Console.ReadLine());
+                    n.RetirarDinero(valorRetiro);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Gracias por utilizar nuestros servicios");
+            }
+            Console.ReadKey();
         }
-        class Persona
+        public class Persona
         {
             public string nombre { get; set; }
 
@@ -26,7 +53,10 @@ namespace Ejercicio02
             private string dni;
             public string DNI
             {
-                get { return dni; } set
+                
+                get { return dni; }
+                //Metodo setter
+                set
                 {
                     if (value.Length < 8)
                     {
